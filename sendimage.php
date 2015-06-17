82fc3704dc2d16a32995c23fc5e58e9330b09bf4
Johnny <tranjohnny64@gmail.com>
2015-06-17 15:24:58 -0500

    Cleaned up sendimage.php file

--- sendimage.php
+++ sendimage.php
@@ -1,28 +1,19 @@
 <?php
 	require("config.inc.php");
-/*
-	if(empty($_REQUEST)) {
 	
-		$fp = fopen('image.png', 'r');
-		$fileData = fread($fp, filesize('image.png'));
-		echo $fileData;
-	}*/
-	//else {
-		// Get image string posted from Android App
-		$base=$_REQUEST['image'];
-		// Decode Image
-		$binary=base64_decode($base);
-		header('Content-Type: bitmap; charset=utf-8');
-			
-		$file = fopen('image.png', 'wb');
-		// Create File
-		fwrite($file, $binary);
-		fclose($file);
-		echo 'Image upload complete, Please check your php file directory';
+	// Get image string posted from Android App
+	$base=$_REQUEST['image'];
+	// Decode Image
+	$binary=base64_decode($base);
+	header('Content-Type: bitmap; charset=utf-8');
 		
-		$fp = fopen('testfile.txt', 'w');
-		fwrite($fp, $binary);
-		fclose($fp);
-		
-	//}
+	$file = fopen('image.png', 'wb');
+	// Create File
+	fwrite($file, $binary);
+	fclose($file);
+	echo 'Image upload complete, Please check your php file directory';
+	
+	$fp = fopen('testfile.txt', 'w');
+	fwrite($fp, $binary);
+	fclose($fp);
 ?>
