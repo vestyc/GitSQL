fe6970793cfcc5227023f5aaaf587a49d5e1e49c
kaif15 <aaqif.1991@gmail.com>
2015-06-25 17:16:06 +0600

    Got the data but for some reason the android can't turn it into a JsonObject
    See if you can make any headway on it. I'll work again tomorrow

--- test.php
+++ test.php
@@ -1,19 +1,19 @@
-<?php	
-	
-	//add code to retrieve bitmap from MySQL database
-	
+<?php
+
 	$con=mysql_connect("localhost","kaif15","real madrid");
     mysql_select_db("flyers",$con);
-    $qry="SELECT * FROM bulletin ORDER BY PostDate DESC LIMIT 1";
+    $qry="SELECT * FROM bulletin ORDER BY PostDate DESC LIMIT 2";
     $result=mysql_query($qry,$con);	
 	
-	$sqldata=mysql_fetch_array($result)
-		
+	$sqldata=mysql_fetch_array($result);
+	$sqldata=mysql_fetch_array($result);
 	$data = array('name' => $sqldata['name'], 'data' =>$sqldata['flyer'], 'date' => $sqldata['PostDate']);
 	
+	echo '<img height="480" width="640" src="data:image;base64,'.$data['data'].' "> ';
 	//Putting data into json format and echoing back to android
 	$data = json_encode($data);
 	echo $data;
 	
 	mysql_close($con);
+
 ?>
