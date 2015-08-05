28a119de96a26685140add5e39b5c08af8590035
Johnny <tranjohnny64@gmail.com>
2015-08-05 12:35:21 -0500

    repo update

--- getdata.php
+++ getdata.php
@@ -1,19 +1,51 @@
 <?php	
 	
-	//add code to retrieve bitmap from MySQL database
+	//added code to retrieve bitmap from MySQL database
+	
+	$index = $_REQUEST['index'];
+	
+	//Retrieving the latest uploaded record to database
+	//Index is the number of records to skip
+	
 	
 	$con=mysql_connect("localhost","kaif15","real madrid");
     mysql_select_db("flyers",$con);
-    $qry="SELECT * FROM bulletin ORDER BY PostDate DESC LIMIT 1";
+	
+	//Number of Records in Database
+	$qry="SELECT COUNT(*) FROM bulletin";
     $result=mysql_query($qry,$con);	
+	$sqldata=mysql_fetch_array($result);
 	
-	$sqldata=mysql_fetch_array($result)
+	$records = $sqldata[0];
+	$records = $records - 3;
+
+	if($index<=$records){
+		$qry="SELECT * FROM bulletin ORDER BY id DESC LIMIT 3";
+		$result=mysql_query($qry,$con);	
+	
+		$sqldata=mysql_fetch_array($result);
 		
-	$data = array('name' => $sqldata['name'], 'data' =>$sqldata['flyer'], 'date' => $sqldata['PostDate']);
+		$i = 0;
 	
-	//Putting data into json format and echoing back to android
-	$data = json_encode($data);
-	echo $data;
+		while($sqldata=mysql_fetch_array($result)){
+			$data[$i] =  array('name' => $sqldata['name'], 'data' =>$sqldata['flyer'], 'date' => $sqldata['PostDate']);
+			$i++;
+		}
+
+		//Putting data into json format and echoing back to android
+		$data = json_encode($data);
+		echo $data;
+		
+		mysql_close($con);
+	}
+    
+	else{
+		$data = array('name' => 'database_full', 'data' =>'data', 'date' => 'date');
+		$data = json_encode($data);
+		echo $data;
+		
+		mysql_close($con);
+	}
 	
-	mysql_close($con);
+	
 ?>
--- test.php
+++ test.php
@@ -2,18 +2,19 @@
 
 	$con=mysql_connect("localhost","kaif15","real madrid");
     mysql_select_db("flyers",$con);
-    $qry="SELECT * FROM bulletin ORDER BY PostDate DESC LIMIT 2";
+    $qry="SELECT * FROM bulletin ORDER BY id DESC LIMIT 3";
     $result=mysql_query($qry,$con);	
 	
-	$sqldata=mysql_fetch_array($result);
-	$sqldata=mysql_fetch_array($result);
-	$data = array('name' => $sqldata['name'], 'data' =>$sqldata['flyer'], 'date' => $sqldata['PostDate']);
+	$i = 0;
 	
-	echo '<img height="480" width="640" src="data:image;base64,'.$data['data'].' "> ';
-	//Putting data into json format and echoing back to android
+	while($sqldata=mysql_fetch_array($result)){
+		$data[$i] =  array('name' => $sqldata['name'], 'data' =>$sqldata['flyer'], 'date' => $sqldata['PostDate']);
+		$i++;
+	}
+	
 	$data = json_encode($data);
+	
 	echo $data;
 	
 	mysql_close($con);
-
 ?>
